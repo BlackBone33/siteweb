920bf1e4ea1f27ada9c572df35e335c9603f29f9
BlackBone33 <anassmm148@gmail.com>
2019-09-11 17:47:53 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+/*------------------------------------------------ sildes show --------------------------------------------------*/
+let index=1;
+moveslide(target);
+
+function plusindex(n){
+    moveslide(index+=n);
+}
+
+function moveslide(target){
+    
+}
